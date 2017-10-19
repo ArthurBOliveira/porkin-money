ecdf70dfa73e65cda89c74508d7d81bd759d62f1
Arthur B. de Oliveira <arthurboliveira93@gmail.com>
2017-10-19 17:35:34 -0200

    New features added

--- Porkin-Money/Assets/Scripts/Coin.cs
+++ Porkin-Money/Assets/Scripts/Coin.cs
@@ -1,6 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
-    
+    public int value;
+
+    private GameController gc;
+    private Text txtCoins;
+
+    private void Awake()
+    {
+        gc = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GameController>();
+        txtCoins = GameObject.FindGameObjectWithTag("txtCoins").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            gc.addCoins(value);
+            int coins = gc.GetCoins();
+            txtCoins.text = coins.ToString();
+
+            GameObject.Destroy(gameObject);
+        }
+    }
 }
--- Porkin-Money/Assets/Scripts/GameController.cs
+++ Porkin-Money/Assets/Scripts/GameController.cs
@@ -9,14 +9,17 @@ public class GameController : MonoBehaviour
 
     public GameObject coin;
 
+    private int coins;
+
     private void Start()
     {
+        coins = 0;
         StartCoroutine(SpawnWaves());
     }
 
     private IEnumerator SpawnWaves()
     {
-        int rndPlat, rndY, fixedX = 5;
+        int rndPlat, rndY, rndCoin, fixedX = 5;
         GameObject platform = new GameObject();
         Vector2 position, position1;
 
@@ -24,24 +27,30 @@ public class GameController : MonoBehaviour
         {
             rndPlat = Random.Range(0, 90);
             rndY = Random.Range(0, 3);
+            rndCoin = Random.Range(0, 2);
 
             #region Platform Y
             switch (rndY)
             {
                 case 0:
                     position = new Vector2(fixedX, -4.5f);
+                    position1 = new Vector2(fixedX, -4f);
                     break;
                 case 1:
                     position = new Vector2(fixedX, -3f);
+                    position1 = new Vector2(fixedX, -2.5f);
                     break;
                 case 2:
                     position = new Vector2(fixedX, 0);
+                    position1 = new Vector2(fixedX, 0.5f);
                     break;
                 case 3:
-                    position = new Vector2(fixedX, 2.5f);
+                    position = new Vector2(fixedX, 2f);
+                    position1 = new Vector2(fixedX, 2.5f);
                     break;
                 default:
                     position = new Vector2(fixedX, -4.5f);
+                    position1 = new Vector2(fixedX, -4f);
                     break;
             }
 
@@ -62,9 +71,25 @@ public class GameController : MonoBehaviour
             }
             #endregion
 
+            #region Coin Spawn
+            Instantiate(coin, position1, Quaternion.identity);
+            #endregion
+
             GameObject.Destroy(platform, 50);
 
-            yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(2f);     
         }
     }
+
+    #region Public
+    public int GetCoins()
+    {
+        return coins;
+    }
+
+    public void addCoins(int a)
+    {
+        coins += a;
+    }
+    #endregion
 }
